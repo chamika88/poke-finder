897b5eb3c7b05b25b17a16c910dadce5368bf125
Chamikara Dharmasena <chamikaradharmasena@chamikaras-macbook-air.local>
2016-08-13 10:32:46 -0500

    Initial Commit

---
+++ Poke-finderTests/Poke_finderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Poke_finderTests.swift
+//  Poke-finderTests
+//
+//  Created by Chamikara Dharmasena on 8/13/16.
+//  Copyright © 2016 Chamikara Dharmasena. All rights reserved.
+//
+
+import XCTest
+@testable import Poke_finder
+
+class Poke_finderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

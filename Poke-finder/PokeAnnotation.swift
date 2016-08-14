b09e37523ed59dad5272a3ec29ab9b74c14c1e03
Chamikara Dharmasena <chamikaradharmasena@Chamikaras-MacBook-Air.local>
2016-08-14 15:46:16 -0500

    first commit/To be tested on ios 10

---
+++ Poke-finder/PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  Poke-finder
+//
+//  Created by Chamikara Dharmasena on 8/13/16.
+//  Copyright © 2016 Chamikara Dharmasena. All rights reserved.
+//
+
+import Foundation
---
+++ Poke-finder/PokeFinder-Bridging_Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Bridging_Header.h
+//  Poke-finder
+//
+//  Created by Chamikara Dharmasena on 8/13/16.
+//  Copyright © 2016 Chamikara Dharmasena. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+
+#endif /* PokeFinder_Bridging_Header_h */
